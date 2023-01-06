bbd36e06917b93a7b970ea152837463a77497301
sandeeptengale <sandeep.tengale@gmail.com>
2023-01-07 01:18:42 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+Creating this repo to add pratise codes around design patterns
---
+++ src/com/sandeep/designpatern/strategy/App.java
@@ -0,0 +1,7 @@
+package com.sandeep.designpatern.strategy;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Running strategy pattern");
+    }
+}
